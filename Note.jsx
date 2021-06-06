83dd953d40178a781de840bc5f884a2ebfd375b3
chamudithagamage <85456863+chamudithagamage@users.noreply.github.com>
2021-06-07 00:54:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        I am Chamuditha Nimsara,and an undergraduate student.This was an amazing
+        bootcamp taken up by Shaurya Sir. we coverd everything from Scratch
+        including JavaScript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
